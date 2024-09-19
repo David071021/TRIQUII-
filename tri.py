af3f87e44a8a5445594342ed00d9c125ae96aadd
David071021 <3105426756i@gmail.com>
2024-09-19 12:59:44 -0500

    Se mejoro lo visual del tablero

--- tri.py
+++ tri.py
@@ -5,17 +5,16 @@ tablero =(
  )
 
 def imprimir_tablero(tab):
-    print('    0 1 2')
+    print('    0   1   2')
     for i, fila in enumerate (tab):
         print(i, end=' | ')
         for colum in  fila:
-            print(colum, end=' ')
-        print('|',i)
-    print('    0 1 2')    
+            print(colum, end=' | ')
+        print(i)
+        if i <2:
+            print('  -------------')
+    print('    0   1   2')    
 
-        
-
-    
 imprimir_tablero(tablero)    
 
     
